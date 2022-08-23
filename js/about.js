bbb0ab836222791b803d0f3c422e2f07428d8ca4
Antu-code <jibonoshomapto3@gmail.com>
2022-08-23 16:05:25 +0600

    more-update

--- js/about.js
+++ js/about.js
@@ -9,3 +9,28 @@ navbar underline hover
         
     }
   }
+
+
+  let thumbnails = document.getElementsByClassName('thumbnail');
+let slider = document.getElementById('slider');
+
+let buttonRight = document.getElementById('slide-right');
+let buttonLeft = document.getElementById('slide-left');
+
+buttonLeft.addEventListener('click', function(){
+    slider.scrollLeft -= 125;
+})
+
+buttonRight.addEventListener('click', function(){
+    slider.scrollLeft += 125;
+})
+
+const maxScrollLeft = slider.scrollWidth - slider.clientWidth;
+
+
+  
+  buttonRight.addEventListener('click', function(){
+      slider.scrollLeft += 125;
+  })
+  
+
