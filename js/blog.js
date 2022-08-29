368af0ea0cf61f4654024dde7ccdb9a94a9b80f6
Antu-code <jibonoshomapto3@gmail.com>
2022-08-29 21:54:03 +0600

    another-update

--- js/blog.js
+++ js/blog.js
@@ -141,22 +141,4 @@ $('.list').click(function(){
 
 
 
-/****blog-details-page****/
-
-let slides = document.querySelectorAll('.nContainer');
-let index = 0;
-
-function next(){
-  slides[index].classList.remove('nActive');
-  index  = (index + 1) % slides.length;
-  slides[index].classList.add('nActive');
-
-};
-
-function prev(){
-  slides[index].classList.remove('nActive');
-  index  = (index - 1 + slides.length) % slides.length;
-  slides[index].classList.add('nActive');
-
-}
 
